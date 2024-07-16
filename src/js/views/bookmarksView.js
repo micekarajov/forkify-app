455d7d87fa10cab217aee2ae189275e7d8164233
Mice Karajov <38554954+micekarajov@users.noreply.github.com>
2024-07-16 15:18:45 +0200

    Minor changes

--- src/js/views/bookmarksView.js
+++ src/js/views/bookmarksView.js
@@ -3,20 +3,17 @@ import previewView from './previewView.js';
 import icons from 'url:../../img/icons.svg';
 
 class BookmarksView extends View {
-    _parentElement = document.querySelector('.bookmarks__list');
-    _errorMessage = 'No bookmarks yet. Find your favourite recepies.';
-    _successMessage = '';
+  _parentElement = document.querySelector('.bookmarks__list');
+  _errorMessage = 'No bookmarks yet. Find your favourite recepies.';
+  _successMessage = '';
 
-    addHandlerRender(handler) {
-        window.addEventListener('load', handler);
-    };
+  addHandlerRender(handler) {
+    window.addEventListener('load', handler);
+  }
 
-
-    _generateMarkup() {
-        return this._data
-            .map(result => previewView.render(result, false))
-            .join();
-    };
-} 
+  _generateMarkup() {
+    return this._data.map(result => previewView.render(result, false)).join();
+  }
+}
 
 export default new BookmarksView();
